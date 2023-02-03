410eb89a63c04f264fbbac6dc201ab88dd7b4d72
jaysson1710 <jaysson1710@hotmail.com>
2023-02-03 15:58:13 -0500

    Update install.ps1

--- 00.install/install.ps1
+++ 00.install/install.ps1
@@ -4,10 +4,11 @@ https://docs.microsoft.com/en-us/azure/developer/terraform/get-started-windows-p
 
 #$PSVersionTable.PSVersion
 $path = "c:\apps\"
-iwr  https://releases.hashicorp.com/terraform/1.3.7/terraform_1.3.7_windows_amd64.zip -O "$($path)\terraform.zip"
+mkdir $path
+Start-BitsTransfer -Source "https://releases.hashicorp.com/terraform/1.3.7/terraform_1.3.7_windows_amd64.zip" -Destination "$($path)\terraform.zip"
+#curl  https://releases.hashicorp.com/terraform/1.3.7/terraform_1.3.7_windows_amd64.zip -o "$($path)\terraform.zip"
 Expand-Archive -LiteralPath "$($path)\terraform.zip" -DestinationPath $path -Force
 $env:PATH = $env:PATH + ";" + $path
 ##$env:PATH + ";" + $path >> $PROFILE
-
 
 ## option modify cat $PROFILE
